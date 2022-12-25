980dabb3d48246f30024d91e7e289ad4f4465961
CometAurora <100214169+CometAurora@users.noreply.github.com>
2022-12-26 00:34:21 +1100

    added calculating for megaminxes

--- CubePermutations.py
+++ CubePermutations.py
@@ -1,38 +1,67 @@
-import cmath
-import math
 import mpmath
+
 mpmath.mp.prec = 99999
 mpmath.mp.dps = 99999
+
+def print_result():
+    print()
+    print(int(result))
+    mpmath.mp.dps = 4
+    print()
+    print("as scientific notation:")
+    print()
+    print(result)
+    mpmath.mp.prec = 99999
+    mpmath.mp.dps = 99999
+
 while True:
     print()
-    a = int(input("Enter Rubik's Cube Size:\n"));
-    if(a % 2 == 0):
-        print()
-        b = (mpmath.power(a - 2, 2) * 6);
-        c = ((a - 2) * 4 * 3);
-        d = (b + c) / 24;
-        e = ((b / 24) * 6) + 1;
-        z = (mpmath.factorial(8) * mpmath.power(3, 7) * mpmath.power(mpmath.factorial(24), d)) / mpmath.power(24, e);
+    faces = int(input("Enter number of faces: (6 for cube, 12 for megaminx)\n"));
+
+    if(faces == 6): #cube
+        size = int(input("Enter number of divisions: (e.g. 2 for pocket cube, 3 for regular rubik's cube, 5 for professors rubik's cube)\n"))
+        centres = (mpmath.power(size - 2, 2) * faces);
+        edges = ((size - 2) * 4 * (faces / 2));
+        if(size < 2):
+            print()
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " rubik's cube is:")
+            print()
+            print(1)
+        elif(size % 2 == 0): #even
+            print()
+            sets = (centres + edges) / 24;
+            nsets = ((centres / 24) * faces) + 1;
+            result = (mpmath.factorial(8) * mpmath.power(3, 7) * mpmath.power(mpmath.factorial(24), sets)) / mpmath.power(24, nsets);
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " rubik's cube is:")
+            print_result()
+        elif(size % 2 == 1): #odd
+            print()
+            sets = ((centres - faces) + (edges - 12)) / 24;
+            nsets = (((centres - faces) / 24) * 6);
+            result = (mpmath.factorial(8) * mpmath.power(3, 7) * mpmath.factorial(12) * mpmath.power(2, 10) * mpmath.power(mpmath.factorial(24), sets)) / mpmath. power(24, nsets)
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " rubik's cube is:")
+            print_result()
+
+    elif(faces == 12): #megaminx
+        size = int(input("Enter number of divisions: (e.g. 2 for kilominx, 3 for megaminx, 5 for gigaminx)\n"))
+        if(size < 2):
+            print()
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " megaminx is:")
+            print()
+            print(1)
+        elif(size == 2):
+            result = (mpmath.factorial(20) * mpmath.power(3, 20) / (2 * 3 * 60));
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " megaminx is:")
+            print_result()
+        elif(size % 2 == 0): #even
+            edges = (((size - 2) * 5 * faces) / 2);
+            result = (((mpmath.factorial(19) / 2) * mpmath.power(3, 18)) * (mpmath.factorial(edges) / 2) * (mpmath.factorial(60) / mpmath.power(mpmath.factorial(5), 12))); # ((mpmath.factorial(edges) / 2) * mpmath.power(2, (edges - 1))) * ((mpmath.factorial(20) / 2) * mpmath.power(3, 19)) / (2 * 3 * 60)
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " megaminx is:")
+            print_result()
+        elif(size % 2 == 1): #odd
+            sized = int((size - 1) / 2);
+            result = (mpmath.factorial(30) * mpmath.factorial(20) * mpmath.power(mpmath.factorial(60), (mpmath.power(sized, 2) - 1)) * mpmath.power(2, (28 - sized)) * mpmath.power(3, 19)) / (mpmath.power(mpmath.factorial(5), (12 * sized * (sized - 1))));
+            print("The number of possible permutations for a " + str(size) + "x" + str(size) + "x" + str(size) + " megaminx is:")
+            print_result()
     else:
-        print()
-        b = (mpmath.power(a - 2, 2) * 6);
-        c = ((a - 2) * 4 * 3);
-        d = ((b - 6) + (c - 12)) / 24;
-        e = (((b - 6) / 24) * 6);
-        z = (mpmath.factorial(8) * mpmath.power(3, 7) * mpmath.factorial(12) * mpmath.power(2, 10) * mpmath.power(mpmath.factorial(24), d)) / mpmath. power(24, e)
-    if(a < 2):
-        print()
-        print("The number of possible permutations for a " + str(a) + "x" + str(a) + "x" + str(a) + " rubik's cube with size is:")
-        print()
-        print(1)
-    else:
-        print("The number of possible permutations for a " + str(a) + "x" + str(a) + "x" + str(a) + " rubik's cube with size is:")
-        print()
-        print(int(mpmath.mpf(z)))
-        mpmath.mp.dps = 4
-        print()
-        print("as scientific notation:")
-        print()
-        print(z)
-        mpmath.mp.prec = 99999
-        mpmath.mp.dps = 99999
+        print("ERROR: invalid input")
